ce5aebfb049bfceb8101b2de24fb3281418696e8
omidhldn <omidhldn@gmail.com>
2023-04-16 20:51:59 +0330

    1

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,35 @@
+import classes from './Profile.module.css';
+
 const ProfilePage = () => {
   return (
-    <h1>Profile</h1>
-    // <h1>
-    //   Alexis Gardin,{" "}
-    //   <span class="italic-green-text">UX UI &nbsp;&nbsp;&nbsp;</span>
-    //   <span class="span-image">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-    //   <span class="italic-green-text">&nbsp;&nbsp;Designer</span> &amp; Expert{" "}
-    //   <span class="italic-green-text">Webflow</span>
-    // </h1>
+    <main className={classes['main-container']}>
+      <div className={classes.container}>
+        <header>
+          <h1>
+            Hello! <br />
+            This is Haniye
+          </h1>
+        </header>
+        <div className={classes.introduction}>
+          <p>
+            I am a Product Designer who creates digital solutions that are
+            visually stunning and user-focused.
+          </p>
+        </div>
+        <div className={classes.haniye}>
+          <img src='./image/profile.png' />
+          <span>I am in my second year of study.</span>
+        </div>
+        <div className={classes.like}>
+          <span
+            >I like to <span className={classes.design}>Design</span>,
+            <span className={classes.paint}>Paint</span>,
+            <span className={classes.write}>write</span> and do some
+            <span className={classes.coding}>coding</span>.</span
+          >
+        </div>
+      </div>
+    </main>
   );
 };
 
